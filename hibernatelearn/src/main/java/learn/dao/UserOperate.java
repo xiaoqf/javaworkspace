5757dd3e2d0e5e0629d497546203457ac0e1f23d
xiaoqf <324215332@qq.com>
2014-11-09 01:54:35 +0800

    用户名查询实现

--- hibernatelearn/src/main/java/learn/dao/UserOperate.java
+++ hibernatelearn/src/main/java/learn/dao/UserOperate.java
@@ -16,11 +16,23 @@ public class UserOperate {
 
     public User getUser(Integer userId) {
         Session loadSession = factory.openSession();
-        loadSession.beginTransaction();
+        Transaction tx = loadSession.beginTransaction();
         User user = (User) loadSession.get(User.class, userId);
+        tx.commit();
         loadSession.close();
         return user;
     }
+    
+	public User getUser(String userName) {
+		Session loadSession = factory.openSession();
+		Transaction tx = loadSession.beginTransaction();
+		User user = (User) loadSession
+				.createQuery("from User as user where user.userName=?")
+				.setString(0, userName).uniqueResult();
+		tx.commit();
+		loadSession.close();
+		return user;
+	}
 
     public boolean delUser(Integer userId) {
         Session delSession = factory.openSession();
@@ -35,11 +47,29 @@ public class UserOperate {
             return false;
         }
     }
+    
+    public boolean delUser(String userName) {
+        Session delSession = factory.openSession();
+        try {
+            Transaction tx = delSession.beginTransaction();
+            delSession.delete(getUser(userName));
+            delSession.flush();
+            tx.commit();
+            delSession.close();
+            return true;
+        } catch (Exception e) {
+            delSession.close();
+            return false;
+        }
+    }
 
     public boolean insUser(User user) {
         Session insSession = factory.openSession();
         try {
             Transaction tx = insSession.beginTransaction();
+            if (getUser(user.getUserName()) != null) {
+            	return false;
+            }
             insSession.save(user);
             tx.commit();
             insSession.close();
--- hibernatelearn/src/main/java/learn/main/testMain.java
+++ hibernatelearn/src/main/java/learn/main/testMain.java
@@ -1,45 +1,33 @@
 package learn.main;
 
+
 import learn.dao.UserOperate;
 import learn.dto.User;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
 
 public class testMain {
 
-    @SuppressWarnings("deprecation")
-    public static void main(String[] args) {
+	public static void main(String[] args) {
         Configuration config = new Configuration().configure();
-        SessionFactory factory = config.buildSessionFactory();
+        ServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(config.getProperties()).build();
+        SessionFactory factory = config.buildSessionFactory(registry);
         UserOperate operate = new UserOperate(factory);
         User user = new User();
         user.setUserName("xiaoqf123");
         user.setPassword("xiaoqf123");
         operate.insUser(user);
-        user = operate.getUser(1);
-        System.out.println(operate.getUser(1));
+        user = operate.getUser("xiaoqf123");
+        System.out.println(user);
         user.setUserName("xiaoabc");
         operate.updUser(user);
-        user = operate.getUser(1);
-        System.out.println(operate.getUser(1));
-        operate.delUser(1);
-        user = operate.getUser(1);
-        System.out.println(operate.getUser(1));
-        // try {
-        // // 加载MySql的驱动类
-        // Class.forName("org.mariadb.jdbc.Driver");
-        // String url = "jdbc:mysql://localhost:3306/learn";
-        // String username = "root";
-        // String password = "xiaoqf@lkg1988";
-        // Connection con = DriverManager.getConnection(url, username, password);
-        //
-        // Statement stmt = con.createStatement();
-        // ResultSet rs = stmt.executeQuery("select * from Users");
-        // System.out.println(rs);
-        // } catch (Exception e) {
-        // e.printStackTrace();
-        // }
+        System.out.println(operate.getUser("xiaoabc"));
+        operate.delUser("xiaoabc");
+        System.out.println(operate.getUser("xiaoabc"));
+        factory.close();
     }
 
 }
